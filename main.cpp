4f1a1015ce7546058172ba564a1ab880157bfbe6
hham <hham@leapmotion.com>
2017-03-16 15:53:21 -0700

    clean up some code

--- main.cpp
+++ main.cpp
@@ -18,40 +18,40 @@
 
 int main(int argc, char **argv){
   if (argc < 4)
-    throw std::runtime_error("usage: grayviewer filename width height");
+    throw std::runtime_error("usage: grayviewer filename width height ex)grayviewer grayimage.raw 1280 480");
 
   int image_width;
   int image_height;
 
-  std::string image_path;
-  image_path.assign(argv[1]);
-  std::cout << image_path << std::endl;
-  image_width = atoi(argv[2]);
-  image_height = atoi(argv[3]);
-  std::cout << image_width << "x" << image_height << std::endl;
-  if (SDL_Init(SDL_INIT_VIDEO) != 0){
+  std::ifstream ifs(argv[1], std::ios::binary);
+  image_width = std::stoi(argv[2]);
+  image_height = std::stoi(argv[3]);
+  if (!ifs.good())
+    throw std::runtime_error("file not found");
+
+  std::vector<char> buffer(image_width * image_height * 3 / 2, 0x80);
+  ifs.read(buffer.data(), image_width * image_height);
+  ifs.close();
+
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-    return 1;
+    return -1;
   }
-  SDL_Window *window = SDL_CreateWindow("grayviewer", 100, 100, image_width, image_height, SDL_WINDOW_SHOWN);
+
+  SDL_Window *window = SDL_CreateWindow("grayviewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, image_width, image_height, SDL_WINDOW_SHOWN);
   if (window == nullptr) {
     std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-    return 1;
+    return -1;
   }
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   if (renderer == nullptr) {
     std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-    return 1;
+    return -1;
   }
   SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_YV12, SDL_TEXTUREACCESS_STREAMING, image_width, image_height);
   SDL_Rect sourceRect = {0, 0, image_width, image_height};
   SDL_Rect destRect = {0, 0, image_width, image_height};
 
-  std::ifstream infile;
-  infile.open(image_path.c_str(), std::ios::binary | std::ios::in);
-  std::vector<char> buffer(image_width * image_height * 3 / 2, 0x80);
-  infile.read(buffer.data(), image_width * image_height);
-  infile.close();
 
   SDL_UpdateTexture(texture, &sourceRect, buffer.data(), image_width);
   SDL_RenderCopy(renderer, texture, &sourceRect, &destRect);
